a5e55fc20aca1fa9af91c09fc0112ac665c41fa9
lenciel <lenciel@gmail.com>
2013-08-27 15:17:13 +0800

    update dot files

--- lib/correction.zsh
+++ lib/correction.zsh
@@ -4,6 +4,7 @@ else
   setopt correct_all
   alias man='nocorrect man'
   alias mv='nocorrect mv'
+  alias curl='nocorrect curl'
   alias mysql='nocorrect mysql'
   alias mkdir='nocorrect mkdir'
   alias gist='nocorrect gist'
